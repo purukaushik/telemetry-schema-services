53ba5abd841cc562f154208bdb4bcd02216adaf2
purukaushik <purukaushik@asu.edu>
2016-06-02 16:45:19 -0700

    configurizing git_checkout.py

--- git_checkout.py
+++ git_checkout.py
@@ -2,25 +2,37 @@
 import git, os
 
 #REMOTE_URL = 'git@github.com:mozilla-services/mozilla-pipeline-schemas.git'
-REMOTE_URL = 'https://github.com/purukaushik/mozilla-pipeline-schemas.git'
-
+#REMOTE_URL = 'https://github.com/purukaushik/mozilla-pipeline-schemas.git'
+#REMOTE_URL = 'https://github.com/mreid-moz/mozilla-pipeline-schemas.git'
 # TODO : periodically do this to get updated schemas
+
+#-----GIT CONFIG-----
+config = {
+    "remote_url": "https://github.com/mreid-moz/mozilla-pipeline-schemas.git",
+    "branch" : "common", #default -master
+    "os_dir" : "./mozilla-pipeline-schemas"
+}
+
+
+
+
+
 def gitcheckout():
-    if os.path.isdir('./mozilla-pipeline-schemas'):
+    if os.path.isdir(config['os_dir']):
         print 'DEBUG: directory exists. will pull instead of clone...'
         repo = git.Repo.init('./mozilla-pipeline-schemas')
         origin = repo.remotes.origin
-        fetch_info = origin.fetch()
+        fetch_info = origin.fetch(config['branch'])
         origin.pull()
         print 'DEBUG: done pulling'
         headcommit = repo.head.commit
         import datetime
-        commit_msg = repr(headcommit.author) + "  " + datetime.datetime.fromtimestamp(int(headcommit.authored_date)).strftime('%Y-%m-%d %H:%M:%S') +  "  " + headcommit.message
+        commit_msg = repr(headcommit.branch) + "  " + repr(headcommit.author) + "  " + datetime.datetime.fromtimestamp(int(headcommit.authored_date)).strftime('%Y-%m-%d %H:%M:%S') +  "  " + headcommit.message 
         print 'DEBUG: latest commit: ' + commit_msg
         pass
     else:
-        print 'DEBUG: cloning '+ REMOTE_URL
-        git.Git().clone(REMOTE_URL)
+        print 'DEBUG: cloning '+ config['remote_url']
+        git.Git().clone(config['remote_url'])
 
 
 if __name__ == '__main__':
