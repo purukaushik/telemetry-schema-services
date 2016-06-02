6a81d4abd8b3f17fd57a425e8c731a43f21f00f1
purukaushik <purukaushik@asu.edu>
2016-06-02 10:08:10 -0700

    pull logic implemented

--- git_checkout.py
+++ git_checkout.py
@@ -7,7 +7,16 @@ REMOTE_URL = 'https://github.com/purukaushik/mozilla-pipeline-schemas.git'
 # TODO : periodically do this to get updated schemas
 def gitcheckout():
     if os.path.isdir('./mozilla-pipeline-schemas'):
-        print 'DEBUG: directory exists. skipping cloning...'
+        print 'DEBUG: directory exists. will pull instead of clone...'
+        repo = git.Repo.init('./mozilla-pipeline-schemas')
+        origin = repo.remotes.origin
+        fetch_info = origin.fetch()
+        origin.pull()
+        print 'DEBUG: done pulling'
+        headcommit = repo.head.commit
+        import datetime
+        commit_msg = repr(headcommit.author) + "  " + datetime.datetime.fromtimestamp(int(headcommit.authored_date)).strftime('%Y-%m-%d %H:%M:%S') +  "  " + headcommit.message
+        print 'DEBUG: latest commit: ' + commit_msg
         pass
     else:
         print 'DEBUG: cloning '+ REMOTE_URL
