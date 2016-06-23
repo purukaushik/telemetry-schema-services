2a3a98278fc34209ff9e78003e15723a72cf855e
purukaushik <purukaushik@asu.edu>
2016-06-23 16:51:48 -0700

    cli version 2

--- cli_service.py
+++ cli_service.py
@@ -1,7 +1,7 @@
 """Schema Service CLI.
 Schema retrieval command line utility.
 Usage:
-cli_service.py [-n <namespace>]
+cli_service.py [(-n <namespace>)]
 cli_service.py [(-n <namespace>  -d <doctype>)]
 cli_service.py [(-n <namespace>  -d <doctype> -v <version>)]
 cli_service.py (-h | --help)
@@ -21,17 +21,32 @@ if __name__ == "__main__":
     
     namespace = 'telemetry'
     docType = 'main'
-    if '-n' in arguments:
+    if arguments['-n'] is not None:
         namespace = arguments['-n']
-        if '-d' in arguments:
-            docType = arguments['-d']
-            if '-v' in arguments:
-                version = arguments['-v']
-                import logging
-                logger = logging.getLogger(__name__)
-                if version is not None:
-                    print get_schema_json(namespace,docType,version,logger).read()
-                else:
-                    lst = get_doctypes_versions(namespace,docType,logger)
-                    for needed,u1,u2,u3 in lst:
-                        print needed
+        
+        docType = arguments['-d']
+        version = arguments['-v']
+        import logging
+        logger = logging.getLogger(__name__)
+        lst = get_doctypes_versions(namespace,docType,logger)
+        versions = list()
+        for needed,u1,u2,u3 in lst:
+            versions.append(needed)    
+        if version is not None:
+            if version in versions:
+                print get_schema_json(namespace,docType,version,logger).read()
+            else:
+                print "No such version. Try one of these:\n"
+        else:
+            
+            if len(versions)==0:
+                print "No such docType. Try one of these:\n"
+                lst = get_doctypes_versions(namespace,None,logger)
+                for needed,u1,u2,u3 in lst:
+                    print needed
+            else:
+                print versions
+    else:
+        print "ERROR! Specify Namespace."
+
+        
