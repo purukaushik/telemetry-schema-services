ad5baea4f0b35c1e368ac2e279684a44bba655a8
purukaushik <purukaushik@asu.edu>
2016-06-29 11:26:01 -0700

    making fetches more proper than before

--- git_checkout.py
+++ git_checkout.py
@@ -13,31 +13,42 @@ def gitcheckout():
     checkout(get_config())
     
 def checkout(config):
+    """
+    Checkout from git with values from branch=config['branch'], remote_url=config['remote_url']
+    """
     if os.path.isdir(config['os_dir']):
-        print 'DEBUG: directory exists. will pull instead of clone...'
-        repo = git.Repo.init('./mozilla-pipeline-schemas')
-        origin = repo.remotes.origin
-        try:
-            print "Fetching branch: " + config['branch']
-            fetch_info = origin.fetch(config['branch'])
-            origin.pull(config['branch'])
-            print 'DEBUG: done pulling ' + config['branch']
-            headcommit = repo.head.commit
-            import datetime
-            commit_msg = repr(headcommit.author) + "  " + datetime.datetime.fromtimestamp(int(headcommit.authored_date)).strftime('%Y-%m-%d %H:%M:%S') +  "  " + headcommit.message 
-            print 'DEBUG: latest commit: ' + commit_msg
-            pass
-        except git.exc.GitCommandError:
-            print "Git error. using local"
-            pass
+        fetch_branch(config)
     else:
         print 'DEBUG: cloning '+ config['remote_url']
         try:
             git.Git().clone(config['remote_url'])
+            fetch_branch(config)
         except git.exc.GitCommandError:
             print "Git error. using local"
             pass
-
+        
+def fetch_branch(config):
+    print 'DEBUG: directory exists. will pull instead of clone...'
+    repo = git.Repo.init('./mozilla-pipeline-schemas')
+    origin = repo.remotes.origin
+    try:
+        print "DEBUG: Fetching branch: " + config['branch']
+        origin.fetch(config['branch'])
+        # git checkout branch
+        git.Git('./mozilla-pipeline-schemas').checkout(config['branch'])
+        # git pull origin branch
+        fetch_info =origin.pull(config['branch'])
+        
+        print 'DEBUG: done pulling ' + config['branch']
+        headcommit = fetch_info[0].ref.commit
+        import datetime
+        commit_msg = repr(headcommit.author) + "  " + datetime.datetime.fromtimestamp(int(headcommit.authored_date)).strftime('%Y-%m-%d %H:%M:%S') +  "  " + headcommit.message 
+        print 'DEBUG: latest commit: ' + commit_msg
+        pass
+    except git.exc.GitCommandError:
+        print "Git error. using local"
+        pass
+        
 def get_config():
     return json.load(open('git_config.json'))
 
