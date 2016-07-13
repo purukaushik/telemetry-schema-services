a2d8335db7845e9354b14cb9663049c5807e2ae2
purukaushik <purukaushik@asu.edu>
2016-07-13 16:51:19 -0700

    Adding tests for different /schema/ endpoints

--- tests/test_service.py
+++ tests/test_service.py
@@ -3,36 +3,49 @@ import os
 import mozschemas_service
 import unittest
 import flask
+
+
 class TestServiceApp(unittest.TestCase):
     def setUp(self):
         self.service_app = mozschemas_service.app.test_client()
         self.config = json.load(open('test_config.json'))
 
     def test_file_end_point(self):
+        self.is_endpoint_good('/file/', 'application/json')
+
+    def test_schema_docTypes_versions_end_point(self):
+        self.is_endpoint_good('/schema/', 'application/json')
+
+    def test_schema_docTypes_end_point(self):
+        namespace = self.config['namespace']
+        for docType in self.config['docTypes']:
+            endpoint = '/schema/' + namespace + "/" + docType
+            resp = self.service_app.get(endpoint)
+
+            self.is_response_good(resp, 200, 'text/html')
+
+    def is_endpoint_good(self, endpoint_prefix, mimetype):
         namespace = self.config['namespace'] + "/"
 
         for docTypes, versions in self.config['docType_versions'].items():
 
             for version in versions:
-                filename = docTypes + "." + version +".schema.json"
-                filepath = namespace + filename
-                resp = self.service_app.get("/file/"+filepath)
+                filepath = "/"
+                if endpoint_prefix == '/file/' :
+                    filename = docTypes + "." + version + ".schema.json"
+                    filepath = namespace + filename
+                elif endpoint_prefix == '/schema/' :
+                    filename = docTypes + "/" + version
+                    filepath = namespace + filename
+                resp = self.service_app.get(endpoint_prefix + filepath)
+                self.is_response_good(resp, 200, mimetype)
 
-                # check if resp object is not None
-                self.assertIsNotNone(resp)
-
-                # check if resp is of type Response
-                self.assertIsInstance(resp, flask.Response)
-
-                # check status=200
-                self.assertEqual(resp.status_code,200)
-                # check mimetype=application/json
-                self.assertEquals(resp.mimetype,'application/json')
-
-                # check if resp.response, ie the json is not empty
-                self.assertIsNotNone(resp.response.file)
-                json_file = json.loads(resp.response.file.read())
-                # FIXME: add these back after all schema.json files have ids where they will be hosted
-                # check for id field in response json
-                #self.assertIn('id', json_file, msg="id not in file " + filename)
-                #self.assertIn(filename, json_file['id'])
+    def is_response_good(self, resp, status_code, mimetype):
+        # check if resp object is not None
+        self.assertIsNotNone(resp)
+        # check if resp is of type Response
+        self.assertIsInstance(resp, flask.Response)
+        # check status=200
+        self.assertEquals(resp.status_code, status_code)
+        # check mimetype=application/json
+        self.assertEquals(resp.mimetype, mimetype)
